fe7b732b6840cc8b8fb2885241b4f804dc019a61
HeyHarshdeep <harshdeeps1313@gmail.com>
2026-09-05 03:42:12 +0530

    Set up publisher and consumer for booking service

---
+++ Services/Booking/FlightBookingSystem.Booking.Application/Consumers/NotificationEventConsumer.cs
@@ -0,0 +1,23 @@
+﻿using FlightBookingSystem.BuildingBlocks.Contracts.EventBus.Messages;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystem.Bookings.Application.Consumers
+{
+    public class NotificationEventConsumer : IConsumer<NotificationEvent>
+    {
+        public async Task Consume(ConsumeContext<NotificationEvent> context)
+        {
+            var notificationEvent = context.Message;
+
+            Console.WriteLine($"Received Notification Event: Recipient={notificationEvent.Recipient}, " +
+                              $"Message={notificationEvent.Message}, Type={notificationEvent.Type}");
+
+            await Task.CompletedTask;
+        }
+    }
+}
--- Services/Booking/FlightBookingSystem.Booking.Application/Handlers/CreateBookingHandler.cs
+++ Services/Booking/FlightBookingSystem.Booking.Application/Handlers/CreateBookingHandler.cs
@@ -1,6 +1,9 @@
 ﻿using FlightBookingSystem.Bookings.Application.Commands;
 using FlightBookingSystem.Bookings.Core.Entities;
 using FlightBookingSystem.Bookings.Core.Repositories;
+using FlightBookingSystem.BuildingBlocks.Contracts.EventBus.Messages;
+using MassTransit;
+using MassTransit.Transports;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,9 +16,11 @@ namespace FlightBookingSystem.Bookings.Application.Handlers
     public class CreateBookingHandler : IRequestHandler<CreateBookingCommand, Guid>
     {
         private readonly IBookingRepository _repository;
-        public CreateBookingHandler(IBookingRepository repository)
+        private readonly IPublishEndpoint _publishEndpoint;
+        public CreateBookingHandler(IBookingRepository repository, IPublishEndpoint publishEndpoint)
         {
             _repository = repository;
+            _publishEndpoint = publishEndpoint;
         }
         public async Task<Guid> Handle(CreateBookingCommand command, CancellationToken cancellationToken)
         {
@@ -29,6 +34,15 @@ namespace FlightBookingSystem.Bookings.Application.Handlers
             };
 
             await _repository.AddBookingAsync(booking);
+
+            // Publish FlightBookedEvent
+            await _publishEndpoint.Publish(new FlightBookedEvent(
+                booking.Id,
+                booking.FlightId,
+                booking.PassengerName,
+                booking.SeatNumber,
+                booking.BookingDate
+                ));
 
             return booking.Id;
         }
